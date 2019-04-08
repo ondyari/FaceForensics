8fbd15ff36935cead3df3c171fc1658efe11da5e
Andreas Roessler <andreas.roessler@tum.de>
2019-04-08 18:33:51 +0200

    Added missing transforms

---
+++ classification/dataset/transform.py
@@ -0,0 +1,23 @@
+"""
+
+Author: Andreas Rössler
+"""
+from torchvision import transforms
+
+xception_default_data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5]*3, [0.5]*3)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5] * 3, [0.5] * 3)
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5] * 3, [0.5] * 3)
+    ]),
+}
